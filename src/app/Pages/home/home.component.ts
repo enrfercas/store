7fdc32b3657967d1afd498fb21aa9c536764d8ed
enrfercas <114150760+enrfercas@users.noreply.github.com>
2024-01-30 18:19:59 +0100

    Todas las funciones del home.component

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,4 +1,10 @@
-import { Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { MatListOption, MatSelectionList } from '@angular/material/list';
 import { ICartItem } from 'src/app/Models/cart-item';
 import { IProduct } from 'src/app/Models/product';
@@ -7,34 +13,63 @@ import { CartService } from 'src/app/Services/cart.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  public value: string = 'Search a product';
-  public categories: string[] = ['computer', 'cell phone', 'keyboard'];
+  public searchKey: string = 'Search a product';
+  public categories: string[] = [];
   public products: IProduct[] = [];
+  public dataSource: IProduct[] = [];
+
 
   @ViewChild('menuCategories') menuCategories: MatSelectionList | undefined;
-
 
   constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
-    this.cartService.getItems().subscribe((data)=>{ this.products = data});
+    this.cartService.getItems().subscribe((data) => {
+      this.products = data;
+      this.dataSource = data;
+      console.log('Data', data);
+      this.getCategories();
+    });
+    this.onFilter();
   }
 
-  // Esta función se puede llamar para obtener los valores seleccionados
-  getSelectedCategories(): string[] | void {
-    if (this.menuCategories) {
-      console.log(this.menuCategories.selectedOptions.selected.map((option: MatListOption) => option.value));
-      return this.menuCategories.selectedOptions.selected.map((option: MatListOption) => option.value);
+  onFilter(): void {
+    const filterCategories = this.getSelectedCategories();
+    this.dataSource = this.products;
+    if (filterCategories) {
+      if (filterCategories.length > 0) {
+        let fiteredProducts: IProduct[][] = filterCategories.map((category) =>{
+          let subFiltered = this.products.filter((product)=>{
+            return product.category === category;
+          });
+          return subFiltered;
+        });
+        this.dataSource = fiteredProducts.flat(1);
+      }
     }
   }
 
-  showArray() {
-    let array = this.getSelectedCategories();
-    console.log(array);
+  onSearch(){
+    const _searchKey = this.searchKey.toLowerCase();
+    if(_searchKey !== 'Search a product') {
+      let fiteredProducts: IProduct[] = this.products.filter((product)=>{
+        return product.title.toLowerCase().includes(_searchKey);
+      });
+      this.dataSource = fiteredProducts;
+    }
   }
+
+  onSort(sort:string) {
+    if(sort === 'asc') {
+      this.dataSource.sort((a,b) => a.price - b.price);
+    }else {
+      this.dataSource.sort((a,b) => b.price - a.price);
+    }
+  }
+
   onAddToCart(product: IProduct): void {
     this.cartService.addToCart({
       product: product.img,
@@ -43,5 +78,21 @@ export class HomeComponent implements OnInit {
       quantity: 1,
       id: product.id,
     });
+  }
+
+  getCategories() {
+    this.products.map((product) => {
+      if (!this.categories.includes(product.category)) {
+        this.categories.push(product.category);
+      }
+    });
+    console.log(this.categories);
+  }
+  getSelectedCategories(): string[] | void {
+    if (this.menuCategories) {
+      return this.menuCategories.selectedOptions.selected.map(
+        (option: MatListOption) => option.value
+      );
+    }
   }
 }
