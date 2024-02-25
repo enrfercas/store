3459f2b0bc991821ab532279bbd2ce9a9520059b
framafer <framafer@gmail.com>
2024-02-25 11:51:00 +0100

    Con server2 funcionando pero aumentando las apis para admin

--- server2/src/controllers/auth.controller.js
+++ server2/src/controllers/auth.controller.js
@@ -39,10 +39,19 @@ export const signup = async (req, res) => {
 
 
 export const signin = async (req, res) => {
-    const userFound = await User.findOne({ email: req.body.email }).populate('roles');
-    if (!userFound) return res.status(400).json({ message: 'User not found' });
-    console.log(userFound);
-    const matchPassword = await User.comparePassword(req.body.password, userFound.password);
-    if (!matchPassword) return res.status(401).json({ token: null, message: 'Invalid password' });
-    res.json({token: ''})
+    try {
+        const userFound = await User.findOne({ email: req.body.email }).populate('roles');
+        if (!userFound) return res.status(400).json({ message: 'User not found' });
+        console.log(userFound);
+        const matchPassword = await User.comparePassword(req.body.password, userFound.password);
+        if (!matchPassword) return res.status(401).json({ token: null, message: 'Invalid password' });
+        const token = jwt.sign({ id: userFound._id }, config.SECRET, {
+            expiresIn: 86400 // 24 hours
+        })
+        res.status(200).json({token})
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error en el login. Inténtelo más tarde' });
+    }
+    
 }
